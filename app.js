0b3678148dfeb9a11fbb7bd4978d63fb531d819a
Austin1780 <austinwilliamsmith@gmail.com>
2018-01-09 09:37:14 -0700

    initial setup

---
+++ app.js
@@ -0,0 +1,162 @@
+const express = require("express");
+const app = express();
+
+// ----------------------------------------
+// App Variables
+// ----------------------------------------
+app.locals.appName = "Welcome Emails App";
+
+// ----------------------------------------
+// ENV
+// ----------------------------------------
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+// ----------------------------------------
+// Site Config
+// ----------------------------------------
+app.use((req, res, next) => {
+  res.locals.siteTitle = "Nodemailer Welcome Emails App";
+  next();
+});
+
+// ----------------------------------------
+// Body Parser
+// ----------------------------------------
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// ----------------------------------------
+// Sessions/Cookies
+// ----------------------------------------
+const cookieSession = require("cookie-session");
+
+app.use(
+  cookieSession({
+    name: "session",
+    keys: [process.env.SESSION_SECRET || "secret"]
+  })
+);
+
+app.use((req, res, next) => {
+  res.locals.session = req.session;
+  next();
+});
+
+// ----------------------------------------
+// Flash Messages
+// ----------------------------------------
+const flash = require("express-flash-messages");
+app.use(flash());
+
+// ----------------------------------------
+// Method Override
+// ----------------------------------------
+const methodOverride = require("method-override");
+const getPostSupport = require("express-method-override-get-post-support");
+
+app.use(
+  methodOverride(
+    getPostSupport.callback,
+    getPostSupport.options // { methods: ['POST', 'GET'] }
+  )
+);
+
+// ----------------------------------------
+// Referrer
+// ----------------------------------------
+app.use((req, res, next) => {
+  req.session.backUrl = req.header("Referer") || "/";
+  next();
+});
+
+// ----------------------------------------
+// Public
+// ----------------------------------------
+app.use(express.static(`${__dirname}/public`));
+
+// ----------------------------------------
+// Logging
+// ----------------------------------------
+const morgan = require("morgan");
+const morganToolkit = require("morgan-toolkit")(morgan);
+
+app.use(morganToolkit());
+
+// ----------------------------------------
+// Email Service
+// ----------------------------------------
+const EmailService = require("./services/email");
+
+// ----------------------------------------
+// Routes
+// ----------------------------------------
+app.use("/", (req, res) => {
+  res.render("welcome/index");
+});
+
+app.post("/emails", (req, res, next) => {
+  const options = {
+    from: "from email here",
+    to: req.body.email_options.to,
+    subject: req.body.email_options.subject,
+    text: req.body.email_options.message,
+    html: `<p>${req.body.email_options.message}</p>`
+  };
+
+  EmailService.send(options)
+    .then(result => {
+      req.flash("success", "Sent!");
+      res.render("emails/new", { result });
+    })
+    .catch(next);
+});
+
+// ----------------------------------------
+// Template Engine
+// ----------------------------------------
+const expressHandlebars = require("express-handlebars");
+const helpers = require("./helpers");
+
+const hbs = expressHandlebars.create({
+  helpers: helpers,
+  partialsDir: "views/",
+  defaultLayout: "application"
+});
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+// ----------------------------------------
+// Server
+// ----------------------------------------
+const port = process.env.PORT || process.argv[2] || 3000;
+const host = "localhost";
+
+let args;
+process.env.NODE_ENV === "production" ? (args = [port]) : (args = [port, host]);
+
+args.push(() => {
+  console.log(`Listening: http://${host}:${port}\n`);
+});
+
+if (require.main === module) {
+  app.listen.apply(app, args);
+}
+
+// ----------------------------------------
+// Error Handling
+// ----------------------------------------
+app.use((err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+
+  if (err.stack) {
+    err = err.stack;
+  }
+  res.status(500).render("errors/500", { error: err });
+});
+
+module.exports = app;
